7991a29f6cabf941ba9ba204dfd413f5ba710cb0
Ashwathpat <1.85584743e+08+Ashwathpat@users.noreply.github.com>
2026-08-12 14:59:42 +0000

    Refine light generator transition

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { IdCardController } from './controllers/idCardController.js';
-import { initMarquee } from './marquee.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  initMarquee();
   const app = new IdCardController();
   app.init();
 });
